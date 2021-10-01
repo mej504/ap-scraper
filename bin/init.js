b3cd945b01da8c465e257a99e5656c7875268791
Justin Minyard <jminyard504@gmail.com>
2021-10-01 11:40:40 -0400

    Scraps unit tests for time being

--- bin/init.js
+++ bin/init.js
@@ -1,10 +1,5 @@
 const app = require('../lib/server/server');
-const express = require('express');
-const path = require('path');
 const PORT = process.env.PORT || 3010;
-
-// app.use(express.static(path.join(__dirname, '../build/static/')))
-app.use(express.static(path.join(__dirname, '../build')));
 
 app.listen(PORT, () => {
 	console.log(`App launched on port ${PORT}`);
