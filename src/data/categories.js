758e3a3008cff2c330e09302fbe04840fa6edd0f
Justin Minyard <jminyard504@gmail.com>
2021-10-04 17:04:59 -0400

    Assigns categories to variable before export

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,9 +1,32 @@
-export default [
-	'US News',
-	'Politics',
-	'World News',
-	'Religion',
-	'Entertainment',
-	'Science',
-	'Business'
-]
+const categories = [
+	{
+		name: 'US News',
+		slug: 'us-news'
+	},
+	{
+		name: 'Politics',
+		slug: 'politics'
+	},
+	{
+		name: 'World News',
+		slug: 'world-news'
+	},
+	{
+		name: 'Religion',
+		slug: 'religion'
+	},
+	{
+		name: 'Entertainment',
+		slug: 'entertainment'
+	},
+	{
+		name: 'Science',
+		slug: 'science'
+	},
+	{
+		name: 'Business',
+		slug: 'business'
+	}
+];
+
+export default categories;
