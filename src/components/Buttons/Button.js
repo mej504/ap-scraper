ea7b8c6bd1332b2a9a1d13c742405b937294b53b
Justin Minyard <jminyard504@gmail.com>
2021-10-06 16:52:20 -0400

    Wires up button; adds aria-label

--- src/components/Buttons/Button.js
+++ src/components/Buttons/Button.js
@@ -1,9 +1,35 @@
+import { useEffect } from 'react';
+
 import styles from './buttons.module.scss';
 
-const Button = ({ text }) => {
+const Button = ({ type, text, slug }) => {
+
+	let btnStyle;
+	let apLink = `https://apnews.com/article/${slug}`;
+
+	switch(type) {
+		case 'stretch':
+			btnStyle = styles.btnStretch;
+			break;
+		case 'static':
+			btnStyle = styles.btnStatic;
+			break;
+		default:
+			btnStyle = styles.btnStatic;
+	}
 
 	return (
-		<a className={ styles.btn } href="#">{ text }</a>
+		<a role='button'
+			aria-label='Read this story on Associated Press'
+			className={ btnStyle }
+			href={apLink}
+			rel="noreferrer noopener"
+			target="_blank"
+		>
+
+			{ text }
+
+		</a>
 	)
 
 }
