e20a4910a4562203e8c0635bc7fd73118a0e28c9
Justin Minyard <jminyard504@gmail.com>
2021-10-11 12:57:20 -0400

    Redirects mobile/tablet user to dash on nav title click

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,30 @@
 import styles from './navbar.module.scss';
 
 // Modules
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // Components
 import { Slant as Hamburger } from 'hamburger-react';
 
-export default function NavBar({ title }) {
+
+export default function NavBar({ screenType, title }) {
 
 	const [ isOpen, setOpen ] = useState(false);
+	let history = useHistory();
+	
+	const handleClick = () => {
+		if( screenType === 'mobile' || screenType === 'tablet' ) {
+			return history.push('/');
+		}
+		return;
+	}
 
 	return (
 
 		<nav className={ styles.navBar }>
 
-			<h3>{ title }</h3>
+			<h3 onClick={ handleClick } className={ screenType === 'mobile' || screenType === 'tablet' ? styles.pointer : null }>{ title }</h3>
 
 			<Hamburger
 				rounded
