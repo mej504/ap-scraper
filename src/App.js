40f983ecd54f9c16c8530fd818c47bb6c4dff7bd
Justin Minyard <jminyard504@gmail.com>
2021-10-14 17:48:27 -0400

    Adds documentation; adds menuOpen check to two functions

--- src/App.js
+++ src/App.js
@@ -37,36 +37,81 @@ function App() {
 	const previousCategory = useRef(null);
 	const fetchInProgress = useRef(false);
 
-
+	/**
+	 * Reverts currentStory ref to null
+	 * @return void
+	 * 
+	 */
 	const unsetStory = () => {
 		return currentStory.current = null;
 	}
 
-	const clickIsInModal = ( element ) => {
+	/**
+	 * A recursive function to determine whether the user is clicking inside
+	 * the menu if it's open
+	 * 
+	 * @param HTMLElement | element | The initial click target
+	 * @return Boolean | True if in menu, otherwise false
+	 * 
+	 */
 
-		if( element.className === 'menu-item' ) {
-			return true;
+	const clickIsInMenu = ( element ) => {
+
+		if( menuOpen ) {
+
+			if( element.className === 'menu-item' ) {
+				return true;
+			}
+
+			if( element.className === 'menu' || element.className === 'hamburger-react' ) {
+				return true;
+			} else {
+				if( element.className === undefined || element.id === undefined ) {
+					clickIsInMenu(element.parentElement);
+				}
+				if( element.id === 'root' ) {
+					return false;
+				}
+			}
+			clickIsInMenu( element.parentElement );
+			return false;
+
 		}
 
-		if( element.className === 'menu' || element.className === 'hamburger-react' ) {
-			return true;
-		} else {
-			if( element.className === undefined || element.id === undefined ) {
-				clickIsInModal(element.parentElement);
-			}
-			if( element.id === 'root' ) {
-				return false;
-			}
-		}
-		clickIsInModal( element.parentElement );
 		return false;
 	}
 
+	/**
+	 * Click handler to close menu if it's open
+	 * 
+	 * @param SyntheticEvent | e | React's synthetic click event object
+	 * @return void
+	 * 
+	 */
+
 	const handleClick = ( e ) => {
-		let result = clickIsInModal( e.target );
-		if( !result ) return setMenuOpen(false);
+
+		if( menuOpen ) {
+			let result = clickIsInMenu( e.target );
+			if( !result ) return setMenuOpen(false);
+			return;
+		}
+
 		return;
+
 	}
+
+	/**
+	 * Sets the initial category when a user first visits the site
+	 * 
+	 * Since mobile and some tablet users will only get the categories list when they land
+	 * on the dashboard, no initial category is set.
+	 * 
+	 * @param String | category | Category to be set as current category
+	 * 
+	 * @return void | Sets currentlyViewing ref
+	 * 
+	 */
 
 	const setInitialCategory = ( category ) => {
 
@@ -78,6 +123,7 @@ function App() {
 		// If we're at /hub/:category, set currentlyViewing to the :category param
 		if( category === undefined && currentPath.includes('/hub/') ) {
 			let splitPath = currentPath.split('/');
+			console.log(splitPath);
 
 			// Doing this ensures the correct category is set regardless of environments
 			return splitPath[splitPath.length - 1];
@@ -87,11 +133,10 @@ function App() {
 
 	}
 
-	// Sets initial category based on 
+	// Sets initial category based on current path
 	if( currentPath === `${ROOT}/hub` || currentPath === `${ROOT}/hub/` ) {
 		setInitialCategory('us-news');
 	} 
-
 
 	useEffect(() => {
 
@@ -101,7 +146,7 @@ function App() {
 		}
 
 		/**
-		 * Updates screen size
+		 * Updates screenType state
 		 * @returns void
 		 * 
 		 */
@@ -110,8 +155,8 @@ function App() {
 			let { innerWidth } = window;
 
 			if( innerWidth <= 425 ) return setScreenType('mobile');
-			if( innerWidth > 425 && innerWidth <= 800 ) return setScreenType('tablet');
-			if( innerWidth > 800 && innerWidth <= 1024 ) return setScreenType('laptop');
+			if( innerWidth > 425 && innerWidth < 800 ) return setScreenType('tablet');
+			if( innerWidth >= 800 && innerWidth <= 1024 ) return setScreenType('laptop');
 
 			return setScreenType('desktop');
 
@@ -119,6 +164,7 @@ function App() {
 
 		updateScreen();
 
+		// Closes menu if it's open when a user expands window to 'laptop' or 'desktop' state
 		if( menuOpen ) {
 			if( screenType === 'laptop' || screenType === 'desktop' ) {
 				setMenuOpen(false);
@@ -152,101 +198,100 @@ function App() {
 				""
 			) : screenType === 'mobile' || screenType === 'tablet' ? (
 
-					<RenderView>
+				<RenderView>
 
+					<Switch>
+
+						<Route exact path={ROOT}>
+							<Categories
+								root={ ROOT }
+								previousCategory={ previousCategory }
+								currentlyViewing={ currentlyViewing }
+								screenType={ screenType }
+								availableCategories={ availableCategories }
+							/>
+						</Route>
+
+						<Route exact path={`${ROOT}/hub`} >
+							<Redirect to={`${ROOT}/`} />
+						</Route>
+
+						<Route path={`${ROOT}/hub/:category`}>
+							<NewsListing
+								root={ ROOT }
+								previousCategory={ previousCategory }
+								fetchInProgress={ fetchInProgress }
+								screenType={ screenType }
+								unsetStory={ unsetStory }
+								currentStory={ currentStory }
+								currentStories={ currentStories }
+								currentlyViewing={ currentlyViewing }
+								apiPath={ API_PATH }
+							/>
+						</Route>
+
+						<Route path={`${ROOT}/story/:slug`}>
+							<Article
+								root={ ROOT }
+								screenType={ screenType }
+								currentStory={ currentStory }
+								apiPath={ API_PATH }
+							/>
+						</Route>
+
+					</Switch>
+
+				</RenderView>
+
+			) : (
+				<div className='main-content-container'>
+					<Categories
+						root={ ROOT }
+						previousCategory={ previousCategory }
+						currentlyViewing={ currentlyViewing }
+						screenType={ screenType }
+						availableCategories={ availableCategories }
+					/>
+					<RenderView>
 						<Switch>
 
-							<Route exact path={ROOT}>
-								<Categories
-									root={ ROOT }
-									previousCategory={ previousCategory }
-									currentlyViewing={ currentlyViewing }
-									screenType={ screenType }
-									availableCategories={ availableCategories }
-								/>
+							{/* Root path starts on US News */}
+							<Route exact path={`${ROOT}/`}>
+								<Redirect to={`${ROOT}/hub/us-news`} />
 							</Route>
 
-							<Route exact path={`${ROOT}/hub`} >
-								<Redirect to={`${ROOT}/`} />
+							<Route exact path={`${ROOT}/hub`}>
+								<Redirect to={`${ROOT}/hub/us-news`} />
 							</Route>
 
-							<Route path={`${ROOT}/hub/:category`}>
+							{/* Handler for paths containing a category slug*/}
+							<Route exact path={`${ROOT}/hub/:category`} children={
 								<NewsListing
 									root={ ROOT }
 									previousCategory={ previousCategory }
-									fetchInProgress={ fetchInProgress }
 									screenType={ screenType }
+									fetchInProgress={ fetchInProgress }
 									unsetStory={ unsetStory }
 									currentStory={ currentStory }
 									currentStories={ currentStories }
 									currentlyViewing={ currentlyViewing }
 									apiPath={ API_PATH }
 								/>
-							</Route>
+							} />
 
-							<Route path={`${ROOT}/story/:slug`}>
+							{/* Handler for paths containing a story slug*/}
+							<Route path={`${ROOT}/story/:slug`} children={
 								<Article
 									root={ ROOT }
 									screenType={ screenType }
 									currentStory={ currentStory }
-									apiPath={ API_PATH }
-								/>
-							</Route>
+									apiPath={ API_PATH }/>
+							}/>
 
 						</Switch>
-
 					</RenderView>
-
-				) : (
-					<div className='main-content-container'>
-						<Categories
-							root={ ROOT }
-							previousCategory={ previousCategory }
-							currentlyViewing={ currentlyViewing }
-							screenType={ screenType }
-							availableCategories={ availableCategories }
-						/>
-						<RenderView>
-							<Switch>
-
-								{/* Root path starts on US News */}
-								<Route exact path={`${ROOT}/`}>
-									<Redirect to={`${ROOT}/hub/us-news`} />
-								</Route>
-
-								<Route exact path={`${ROOT}/hub`}>
-									<Redirect to={`${ROOT}/hub/us-news`} />
-								</Route>
-
-								{/* Handler for paths containing a category slug*/}
-								<Route exact path={`${ROOT}/hub/:category`} children={
-									<NewsListing
-										root={ ROOT }
-										previousCategory={ previousCategory }
-										screenType={ screenType }
-										fetchInProgress={ fetchInProgress }
-										unsetStory={ unsetStory }
-										currentStory={ currentStory }
-										currentStories={ currentStories }
-										currentlyViewing={ currentlyViewing }
-										apiPath={ API_PATH }
-									/>
-								} />
-
-								{/* Handler for paths containing a story slug*/}
-								<Route path={`${ROOT}/story/:slug`} children={
-									<Article
-										root={ ROOT }
-										screenType={ screenType }
-										currentStory={ currentStory }
-										apiPath={ API_PATH }/>
-								}/>
-
-							</Switch>
-						</RenderView>
-					</div>
-				)}
-
+				</div>
+			)}
 
 		</main>
 
